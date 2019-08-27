e829bb412f1037173e317d983dad7c0cfd62ec77
Allen He <308709609@qq.com>
2019-08-27 09:43:24 +0800

    lesson 6

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import SHOP_DATA from "./shop.data"
+import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+
+class ShopPage extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            collection: SHOP_DATA,
+        }
+    }
+
+    render() {
+        const {collection} = this.state
+        return (
+            <div className='shop-page'>
+                {collection.map(({id, ...props}) => (
+                    <CollectionPreview key={id} {...props} />
+                ))}
+            </div>
+        )
+    }
+}
+
+export default ShopPage
