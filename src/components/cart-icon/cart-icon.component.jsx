ac057ba28407900a3efd1f984b1938f3c1481858
Allen He <308709609@qq.com>
2019-08-29 11:48:16 +0800

    checkout page

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {createStructuredSelector} from "reselect"
 
 import {toggleCartHidden} from "../../redux/cart/cart.actions"
 import {selectCartItemsCount} from '../../redux/cart/cart.selectors'
@@ -14,8 +15,12 @@ const CartIcon = ({toggleCartHidden, itemCount}) => (
         <span className='item-count'>{itemCount}</span>
     </div>
 )
-const mapStateToProps = state => ({
-    itemCount: selectCartItemsCount(state)
+// const mapStateToProps = state => ({
+//     itemCount: selectCartItemsCount(state)
+// })
+
+const mapStateToProps = createStructuredSelector({
+    itemCount: selectCartItemsCount,
 })
 
 const mapDispatchToProps = dispatch => ({
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import {Link} from "react-router-dom"
 import {connect} from 'react-redux'
+import {createStructuredSelector} from "reselect"
 
 import {auth} from "../../firebase/firebase.utils"
 import CartIcon from "../cart-icon/cart-icon.component"
 import CartDropdown from "../cart-dropdown/cart-dropdown.component"
+import {selectCurrentUser} from "../../redux/user/user.selector"
+import {selectCartHidden} from "../../redux/cart/cart.selectors"
 
 import {ReactComponent as Logo} from '../../assets/crown.svg'
 import './header.styles.scss'
@@ -33,9 +36,14 @@ const Header = ({currentUser, hidden}) => (
     </div>
 )
 
-const mpaStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
-    currentUser,
-    hidden,
+// const mpaStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
+//     currentUser,
+//     hidden,
+// })
+
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser,
+    hidden: selectCartHidden,
 })
 
-export default connect(mpaStateToProps)(Header)
+export default connect(mapStateToProps)(Header)
